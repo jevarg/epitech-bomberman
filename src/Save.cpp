535dcbb74b7dbb454a517a7eacbd9a8960c60db8
Harold Fritsch <fritsc_h@epitech.eu>
2014-05-07 18:35:04 +0200

    Ajout debut classe save

---
+++ src/Save.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include "GameEngine.hpp"
+#include "Save.hpp"
+#include "Container.hpp"
+
+Save::Save()
+{
+  ;
+}
+
+Save::~Save()
+{
+  ;
+}
+
+Save::saveGame(std::vector<Container *> cont, const std::string &name)
+{
+  ;
+}
---
+++ src/Save.hpp
@@ -0,0 +1,23 @@
+//
+// Save.hpp for  in /home/fritsc_h/bomberman
+// 
+// Made by Harold Fritsch
+// Login   <fritsc_h@epitech.net>
+// 
+// Started on  Tue May  6 21:55:22 2014 Harold Fritsch
+// Last update Tue May  6 23:09:15 2014 Harold Fritsch
+//
+
+#ifndef SAVE_HPP__
+# define SAVE_HPP_
+
+class	Save
+{
+public:
+  Save();
+  ~Save();
+private:
+  ;
+};
+
+#endif /* !SAVE_HPP */
