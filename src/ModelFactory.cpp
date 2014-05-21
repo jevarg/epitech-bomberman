2fdf553a4420932b16ac6ff75c3356e6b2ea289f
dellam_a <adrien.dellamaggiora@epitech.eu>
2014-05-21 10:40:45 +0200

    Add some methode to the factory

--- src/ModelFactory.cpp
+++ src/ModelFactory.cpp
@@ -7,13 +7,33 @@ const ModelFactory &ModelFactory::getInstance()
   return (_factory);
 }
 
-ModelFactory::ModelFactory()
+ModelFactory::ModelFactory(): _texture(), _model()
 {
 
 }
 
 
 ModelFactory::~ModelFactory()
+{
+
+}
+
+void	ModelFactory::addModel(eType type, IObject *obj)
+{
+
+}
+
+void	ModelFactory::addModel(eType type, const std::string &model_file)
+{
+
+}
+
+void	ModelFactory::addModel(eType type, IObject *obj, const std::string &texture_file)
+{
+
+}
+
+void	ModelFactory::addTexture(const std::string &texture_file)
 {
 
 }
--- src/ModelFactory.hpp
+++ src/ModelFactory.hpp
@@ -1,5 +1,13 @@
 #ifndef _MODELFACTORY_HPP_
-#define _MODELFACTORY_HPP_
+# define _MODELFACTORY_HPP_
+
+# include <string>
+# include <vector>
+# include <map>
+# include <Texture.hh>
+# include "Model.hpp"
+# include "IObject.hpp"
+# include "AEntity.hpp"
 
 class ModelFactory
 {
@@ -7,6 +15,12 @@ public:
   /* Appel */
   /* ModelFactory &fact = ModelFactory::getInstance(); */
   static const ModelFactory &getInstance();
+  void	addModel(eType, IObject *);
+  void	addModel(eType, const std::string &);
+  void	addModel(eType, IObject *, const std::string &);
+  void	addTexture(const std::string &);
+
+  IObject	*getModel(eType type);
 
 private:
 
@@ -14,7 +28,8 @@ private:
 
   ModelFactory();
   virtual ~ModelFactory();
-
+  std::map<std::string, gdl::Texture*> _texture;
+  std::map<eType, IObject *> _model;
 };
 
 #endif /* _MODELFACTORY_HPP_ */
