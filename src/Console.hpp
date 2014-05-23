33dae960d68527b0fcd63ce69c9fe3d2709907ce
Harold Fritsch <fritsc_h@epitech.eu>
2014-05-23 14:14:26 +0200

    maj map cmd console

--- src/Console.hpp
+++ src/Console.hpp
@@ -12,6 +12,7 @@ public:
   Console(Settings &set);
   ~Console();
   std::string	&parseCmd(const std::string &);
+
 private:
   Settings	&_set;
   std::map<std::string, std::string &(Console::*)(const std::string &)> _cmd;
