6b45192982ef5705baa0b3265269e4b67302062c
dellam_a <adrien.dellamaggiora@epitech.eu>
2014-05-23 10:19:59 +0200

    Add Flame in the factory

--- src/GameEngine.cpp
+++ src/GameEngine.cpp
@@ -56,6 +56,7 @@ bool GameEngine::initialize()
 
   fact.addModel(WALL, new Cube(*skybox), WALL_TEXTURE);
   fact.addModel(BOX, new Cube(*skybox), BOX_TEXTURE);
+  fact.addModel(FLAME, new Cube(*skybox), FLAME_TEXTURE);
   fact.addModel(CHARACTER, CHARACTER_MODEL);
   fact.addModel(BOMB, BOMB_MODEL);
 
