a95139b56226fd4faaa2dab06bf23c22556383ab
collin_b <collin_b@epitech.eu>
2014-05-29 18:13:36 +0200

    inproove can_i_put_bomb

--- ai/actions.lua
+++ ai/actions.lua
@@ -6,28 +6,28 @@ function can_i_put_bomb(map_nb, x, y, block)
 
 	for i = 1, BOMB_RANGE + 1 do
 		if (block[1] == 0) then
-			if (cur_y - i > 0 and map_nb[cur_y - i][cur_x] ~= ".") then block[1] = 1
+			if (cur_y - i < 1 or map_nb[cur_y - i][cur_x] ~= ".") then block[1] = 1
 			elseif (cur_y - i > 0 and map_nb[cur_y - i][cur_x] == ".") then way = 1
 			else break end
 		end
 	end
 	for i = 1, BOMB_RANGE + 1 do
 		if (block[2] == 0) then
-			if (cur_x - i > 0 and map_nb[cur_y][cur_x - i] ~= ".") then block[2] = 1
+			if (cur_x - i < 1 or map_nb[cur_y][cur_x - i] ~= ".") then block[2] = 1
 			elseif (cur_x - i > 0 and map_nb[cur_y][cur_x - i] == ".") then way = 3
 			else break end
 		end
 	end
 	for i = 1, BOMB_RANGE + 1 do
 		if (block[3] == 0) then
-			if (cur_y + i < MAP_YMAX and map_nb[cur_y + i][cur_x] ~= ".") then block[3] = 1
+			if (cur_y + i > MAP_YMAX or map_nb[cur_y + i][cur_x] ~= ".") then block[3] = 1
 			elseif (cur_y + i < MAP_YMAX and map_nb[cur_y + i][cur_x] == ".") then way = 2
 			else break end
 		end
 	end
 	for i = 1, BOMB_RANGE + 1 do
 		if (block[4] == 0) then
-			if (cur_x + i < MAP_XMAX and map_nb[cur_y][cur_x + i] ~= ".") then block[4] = 1
+			if (cur_x + i > MAP_XMAX or map_nb[cur_y][cur_x + i] ~= ".") then block[4] = 1
 			elseif (cur_x + i < MAP_XMAX and map_nb[cur_y][cur_x + i] == ".") then way = 4
 			else break end
 		end
@@ -40,6 +40,7 @@ function can_i_put_bomb(map_nb, x, y, block)
 end
 
 function run_out_danger(map, x, y)
+	local nb = 50
 	local cur_x, cur_y = x, y
 	local way = 0
 	local gotox = {0, 0, -1, 1}
