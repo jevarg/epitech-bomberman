81ed3837d75a7189524d00c1e29cbc85d227fbcb
dellam_a <adrien.dellamaggiora@epitech.eu>
2014-06-08 19:07:16 +0200

    Add the current number of bomb in the HUD

--- src/Hud.cpp
+++ src/Hud.cpp
@@ -2,11 +2,15 @@
 #include "Hud.hpp"
 
 HUD::HUD(gdl::AShader &shader)
-  : _shader(shader), _heart(HEART_MODEL), _noHeart(NOHEART_MODEL), _fps()
+  : _shader(shader), _stock(STOCK_TEXTURE), _nostock(NOSTOCK_TEXTURE),
+    _heart(HEART_TEXTURE), _noHeart(NOHEART_TEXTURE), _fps()
 {
+  _stock.setSize(30, 30);
+  _nostock.setSize(30, 30);
   _heart.setSize(30, 30);
   _noHeart.setSize(30, 30);
-  if (!_fps.initialize() || !_heart.initialize() || !_noHeart.initialize())
+  if (!_fps.initialize() || !_heart.initialize() || !_noHeart.initialize()
+      || !_stock.initialize() || !_nostock.initialize())
     throw(Exception("Cannot Init the HUD"));
 }
 
@@ -34,7 +38,7 @@ void HUD::draw(Player *player, t_gameinfo &gameInfo)
   _shader.setUniform("winX", x);
   _shader.setUniform("winY", y);
   _fps.draw(_shader, *gameInfo.clock);
-  for (int i = 1; i <= HEALTH_MAX;i++)
+  for (int i = 1; i <= HEALTH_MAX;++i)
     {
       if (i <= player->getHealth())
 	{
@@ -48,6 +52,21 @@ void HUD::draw(Player *player, t_gameinfo &gameInfo)
 	  _noHeart.fillGeometry();
 	  _noHeart.draw(_shader, *gameInfo.clock);
 	}
+    }
+  for (int i = 0;i < player->getMaxBomb();++i)
+    {
+      if (i < player->getBombStock())
+  	{
+  	  _stock.setPos((i + 1) * 30, 40);
+  	  _stock.fillGeometry();
+  	  _stock.draw(_shader, *gameInfo.clock);
+  	}
+      else
+  	{
+  	  _nostock.setPos((i + 1) * 30, 40);
+  	  _nostock.fillGeometry();
+  	  _nostock.draw(_shader, *gameInfo.clock);
+  	}
     }
   glEnable(GL_DEPTH_TEST);
 }
