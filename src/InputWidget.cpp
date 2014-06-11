1a53f2d24cd3ef920231fe2045643a526dbea312
Jean Gravier <jean.gravier@epitech.eu>
2014-06-12 00:51:59 +0200

    Fixed Depth menu input text

--- src/InputWidget.cpp
+++ src/InputWidget.cpp
@@ -4,7 +4,7 @@ InputWidget::InputWidget(int x, int y, int height, int width,
 		       const std::string &text) :
   AWidget(x, y, height, width, text)
 {
-  //  setSquare(new Square("assets/singleplayer.tga")); // tmp
+
 }
 
 InputWidget::~InputWidget()
@@ -15,13 +15,11 @@ void	InputWidget::onClick(const t_gameinfo &/*gameInfo*/, Menu &menu)
 {
   std::string	buf;
 
-  glDisable(GL_DEPTH_TEST);
   menu.textInput(buf, 20, _x, _y);
   // here do a treatment with buf, buf contains the user's input.
-  glEnable(GL_DEPTH_TEST);
 }
 
 void	InputWidget::draw(gdl::AShader &shader, const gdl::Clock &clock)
 {
-  
+
 }
