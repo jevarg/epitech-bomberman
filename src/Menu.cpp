835b7d3455a033a30ec7577ffde258fbad1ae56f
Jean Gravier <jean.gravier@epitech.eu>
2014-06-14 17:51:52 +0200

    Added chicken model

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -228,7 +228,7 @@ bool  Menu::initialize()
   fact.addModel(CHARACTER1, CHARACTER_MODEL);
   fact.addModel(CHARACTER2, CHARACTER2_MODEL);
   fact.addModel(BOT, BOT_MODEL);
-  fact.addModel(CHICKEN, BOMB_MODEL);
+  fact.addModel(CHICKEN, CHICKEN_MODEL);
   fact.addModel(BOMB, BOMB_MODEL);
 
   _player1 = new Player(0, 0, &_gameInfo, CHARACTER1);
